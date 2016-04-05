048ce7c58adedbb7ca3209f0ccd7b9f75636c1c3
dours <Oleg.Medvedev@lanit-tercom.com>
2016-04-05 21:14:29 +0300

    vector instructions are described as a class, a sequential execution model is implemented for them; a 3x3 convolution with small koeffs is implemented in the instruction set

---
+++ InstructionSetArchitecture/instruction.h
@@ -0,0 +1,66 @@
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include "common.h"
+#include "processorState.h"
+#include <systemc.h>
+#include <assert.h>
+
+#define INSTRUCTION_WIDTH (3 * (1 + LOG_N_REGS) + 1 + 3 * VECTOR_ALU_WIDTH + 1)
+
+  // a left instruction operand may be a vector from a current window given by its X-coordinate in the window
+  // or it may be a local register of the window given by its number
+  // a right instruction operand may be a local register or a register, which is common for all the windows currently being processed
+  enum tOperandType  { WINDOW, LOCAL, COMMON };
+
+  enum tOperationType { SHORT_MUL, ADD, SUB, SHIFT_IN, CMP, EXTRACT, PRINT, XOR };
+  // Vector kind means that operation is applied to both operands. 
+  // Reduce means that op is applied to pairs of scalars taken from the right operand, while the left one is ignored. 
+  enum tOperationKind { VECTOR, REDUCE };
+
+ 
+/// the class describes an instruction in a programmer-readable form. It can also generate sc_uint bit vector (which is the actual instruction code)
+class Instruction {
+
+  tOperandType lopndk, ropndk, dstk;
+  sc_uint<LOG_N_REGS> lopnd, ropnd, dst;
+
+  tOperationKind opKind;
+  vector<tOperationType> opType;
+
+  sc_uint<DATA_WIDTH> theConstant; 
+  bool finishedThisWindow; 
+
+public:
+
+  // this one creates an instruction in which all the operations are the same
+  Instruction(
+    tOperandType lk, sc_uint<LOG_N_REGS> l, 
+    tOperandType rk, sc_uint<LOG_N_REGS> r,
+    tOperandType dstk, sc_uint<LOG_N_REGS> dst,
+    tOperationKind   opk, tOperationType op,
+    sc_uint<DATA_WIDTH> c, bool f
+  ) {
+    assert(VECTOR_ALU_WIDTH <= N_REGS); // just a simplification, maybe the code may be more generic, maybe not
+    lopndk = lk;
+    lopnd = l;
+    ropndk = rk;
+    ropnd = r;
+    this -> dstk = dstk;
+    this -> dst = dst;
+    opKind = opk;
+    for (int i = 0; i < VECTOR_ALU_WIDTH; ++i) { opType.push_back(op); } 
+    if (SHIFT_IN == op) theConstant = c; else theConstant = 0xbeef;
+    finishedThisWindow = f;
+  }
+
+  void execute(ProcessorState& state);
+  void decode(sc_uint<INSTRUCTION_WIDTH>); // assign the instruction from a coded bit vector
+  sc_uint<INSTRUCTION_WIDTH> encode();
+
+};
+
+
+
+#endif
+
---
+++ InstructionSetArchitecture/pseudoBlur.cpp
@@ -0,0 +1,67 @@
+
+#include "instruction.h"
+
+// this code loads a following 3x3 kernel matrix into common regs of the processor
+// 1 2 1
+// 2 4 2
+// 1 2 1
+vector<Instruction> initCommon;
+void initInitCommon() { 
+  vector<Instruction> a;
+
+  a.push_back(Instruction(LOCAL,  0, LOCAL,  0, LOCAL,  0, VECTOR, XOR,      0, false)); // local_reg[0] := 0
+
+  a.push_back(Instruction(WINDOW, 0, LOCAL , 0, COMMON, 0, VECTOR, SHIFT_IN, 1, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 0, COMMON, 0, VECTOR, SHIFT_IN, 2, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 0, COMMON, 0, VECTOR, SHIFT_IN, 1, false));
+
+  a.push_back(Instruction(WINDOW, 0, LOCAL , 0, COMMON, 1, VECTOR, SHIFT_IN, 2, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 1, COMMON, 1, VECTOR, SHIFT_IN, 4, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 1, COMMON, 1, VECTOR, SHIFT_IN, 2, false));
+
+  a.push_back(Instruction(WINDOW, 0, LOCAL , 0, COMMON, 2, VECTOR, SHIFT_IN, 1, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 2, COMMON, 2, VECTOR, SHIFT_IN, 2, false));
+  a.push_back(Instruction(WINDOW, 0, COMMON, 2, COMMON, 2, VECTOR, SHIFT_IN, 1, false));
+
+  a.push_back(Instruction(LOCAL, 0, LOCAL, 0, LOCAL, 0, VECTOR, PRINT,    0, false));
+  a.push_back(Instruction(COMMON, 0, COMMON, 0, COMMON, 0, VECTOR, PRINT,    0, false));
+  a.push_back(Instruction(COMMON, 1, COMMON, 1, COMMON, 1, VECTOR, PRINT,    0, false));
+  a.push_back(Instruction(COMMON, 2, COMMON, 2, COMMON, 2, VECTOR, PRINT,    0, false));
+
+  initCommon = a;
+}
+
+
+vector<Instruction> computePseudoBlur;
+void initComputePseudoBlur() { 
+  vector<Instruction> a;
+
+  a.push_back(Instruction(LOCAL,  0, LOCAL, 0, LOCAL, 0, VECTOR, XOR, 0, false)); // local_reg[0] := 0  
+
+  a.push_back(Instruction(WINDOW, 0, COMMON, 0, LOCAL, 1, VECTOR, SHORT_MUL, 0, false)); // local_reg[1] := window[0] * common_reg[0]
+  a.push_back(Instruction(LOCAL,  0, LOCAL,  1, LOCAL, 0, VECTOR, ADD,       0, false)); // local_reg[0] += local_reg[1]
+//  a.push_back(Instruction(LOCAL,  0, LOCAL,  0, LOCAL, 0, VECTOR, PRINT,     0, false)); // debug print result
+
+  a.push_back(Instruction(WINDOW, 1, COMMON, 1, LOCAL, 1, VECTOR, SHORT_MUL, 0, false)); // local_reg[1] := window[0] * common_reg[0]
+  a.push_back(Instruction(LOCAL,  0, LOCAL,  1, LOCAL, 0, VECTOR, ADD,       0, false)); // local_reg[0] += local_reg[1]
+//  a.push_back(Instruction(LOCAL,  0, LOCAL,  0, LOCAL, 0, VECTOR, PRINT,     0, false)); // debug print result
+
+  a.push_back(Instruction(WINDOW, 2, COMMON, 2, LOCAL, 1, VECTOR, SHORT_MUL, 0, false)); // local_reg[1] := window[0] * common_reg[0]
+  a.push_back(Instruction(LOCAL,  0, LOCAL,  1, LOCAL, 0, VECTOR, ADD,       0, false)); // local_reg[0] += local_reg[1]
+
+//  a.push_back(Instruction(LOCAL,  0, LOCAL,  0, LOCAL, 0, VECTOR, PRINT,     0, false)); // debug print result
+  a.push_back(Instruction(LOCAL,  2/*reductionStage*/, LOCAL,  0, LOCAL, 0, REDUCE, ADD,       0, false)); // add all components of the vector with two "reduction" additions
+  a.push_back(Instruction(LOCAL,  3/*reductionStage*/, LOCAL,  0, LOCAL, 0, REDUCE, ADD,       0, false)); // 
+
+  a.push_back(Instruction(LOCAL,  0, LOCAL,  0, LOCAL, 0, VECTOR, PRINT,     0, true)); // debug print result
+
+  computePseudoBlur = a;
+}
+
+void runProgram(ProcessorState& ps) { 
+  initInitCommon();
+  initComputePseudoBlur();
+  for (int i = 0; i < initCommon.size(); ++i) initCommon.at(i).execute(ps);
+  while (1) for (int i = 0; i < computePseudoBlur.size(); ++i) computePseudoBlur.at(i).execute(ps);
+}
+
